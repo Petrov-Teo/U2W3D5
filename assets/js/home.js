875b2b327705eb27e1f3afb6ccff39572509241d
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-06-14 15:52:46 +0200

    da completare

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,18 +2,22 @@ const URLPost = "https://strive.school/studentlogin";
 const apiKay =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZjMDVhNjdjMjM5YzAwMTUyZjRiOWIiLCJpYXQiOjE3MTgzNTUzNjYsImV4cCI6MTcxOTU2NDk2Nn0.SNr5-HHqDmixkK6fUBAm5OCVFrek45OIG5kNTm06HDg";
 
-const aricoleForm = document.forms.aricoleForm.addEventListener("submit", function (e) {
-  e.preventDefault;
-});
-
 const newArticole = {
-  name: aricoleForm.nomeArticolo.value,
-  description: aricoleForm.descrizione.value,
-  brand: aricoleForm.brand.value,
-  imgUrl: aricoleForm.imgUrl.value,
-  price: aricoleForm.price.value,
+  name: nomeArticolo.valuee,
+  imgUrl: imgUrl.value,
+  price: price.value,
 };
 const articoles = [];
 articoles.push(newArticole);
+console.log(articoles);
+console.log(newArticole.name.vaue);
 
-console.log(newArticole);
+document.forms["aricoleForm"].addEventListener("submit", function (e) {
+  const nomeArticolo = document.getElementById("nomeArticolo");
+  const description = document.getElementById("description");
+  const brand = document.getElementById("brand");
+  const imgUrl = document.getElementById("imgUrl");
+  const price = document.getElementById("price");
+
+  e.preventDefault();
+});
