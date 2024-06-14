8f40262bc437a5d8fdd5683bcb6cfc60f0e63fde
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-06-14 16:58:55 +0200

    corretto array

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,22 +2,21 @@ const URLPost = "https://strive.school/studentlogin";
 const apiKay =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZjMDVhNjdjMjM5YzAwMTUyZjRiOWIiLCJpYXQiOjE3MTgzNTUzNjYsImV4cCI6MTcxOTU2NDk2Nn0.SNr5-HHqDmixkK6fUBAm5OCVFrek45OIG5kNTm06HDg";
 
-const newArticole = {
-  name: nomeArticolo.valuee,
-  imgUrl: imgUrl.value,
-  price: price.value,
-};
 const articoles = [];
-articoles.push(newArticole);
-console.log(articoles);
-console.log(newArticole.name.vaue);
 
 document.forms["aricoleForm"].addEventListener("submit", function (e) {
+  e.preventDefault();
   const nomeArticolo = document.getElementById("nomeArticolo");
   const description = document.getElementById("description");
   const brand = document.getElementById("brand");
   const imgUrl = document.getElementById("imgUrl");
   const price = document.getElementById("price");
 
-  e.preventDefault();
+  const newArticole = {
+    name: nomeArticolo.value,
+    imgUrl: imgUrl.value,
+    price: price.value,
+  };
+  articoles.push(newArticole);
+  console.log(articoles);
 });
