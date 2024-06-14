8571acf71037a8715e04123c502be05dce6b4cc8
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-06-14 18:13:30 +0200

    working progress

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,22 +1,27 @@
-const URLPost = "https://strive.school/studentlogin";
+const URLPost = "https://striveschool-api.herokuapp.com/api/product/";
 const apiKay =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZjMDVhNjdjMjM5YzAwMTUyZjRiOWIiLCJpYXQiOjE3MTgzNTUzNjYsImV4cCI6MTcxOTU2NDk2Nn0.SNr5-HHqDmixkK6fUBAm5OCVFrek45OIG5kNTm06HDg";
-
-const articoles = [];
 
 document.forms["aricoleForm"].addEventListener("submit", function (e) {
   e.preventDefault();
   const nomeArticolo = document.getElementById("nomeArticolo");
-  const description = document.getElementById("description");
+  const description = document.getElementById("descrizione");
   const brand = document.getElementById("brand");
   const imgUrl = document.getElementById("imgUrl");
   const price = document.getElementById("price");
 
   const newArticole = {
     name: nomeArticolo.value,
+    description: description.value,
+    brand: brand.value,
     imgUrl: imgUrl.value,
     price: price.value,
   };
-  articoles.push(newArticole);
-  console.log(articoles);
+  fetch(URLPost, {
+    method: "POST",
+    body: JSON.stringify(newArticole),
+    headers: {
+      Authorization: "Bearer " + apiKay,
+    },
+  });
 });
